e289c60c8484d2c7c3dfcf165d6708a34034002a
TmTron <martin.trummer@tmtron.com>
2021-12-09 17:27:55 +0100

    minor

--- apps/nest-app/src/app/my.dto.ts
+++ apps/nest-app/src/app/my.dto.ts
@@ -1,7 +1,7 @@
 import {Temporal} from "@js-temporal/polyfill";
 
 export class MyDto {
-    id: string;
-    count: number;
+    id!: string;
+    count!: number;
     dateTime = Temporal.Now.instant().toString()
 }
