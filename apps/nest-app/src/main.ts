50a46f0728b9b7f2121ec0e73ba6b4b6fcefeaaa
TmTron <martin.trummer@tmtron.com>
2021-12-09 16:14:23 +0100

    initial commit * default nx nest project * added nestjs/swagger

---
+++ apps/nest-app/src/main.ts
@@ -0,0 +1,33 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app/app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+      .setTitle('Cats example')
+      .setDescription('The cats API description')
+      .setVersion('1.0')
+      .addTag('cats')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('oapi', app, document);
+  
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3333;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+}
+
+bootstrap().catch(e => console.error(`failed to bootstrap app`, e))
